8bc6e1d810e51a2fbc9942b0434ebfc1660b0ed8
GeonwooKim <48337874+GeonwooVincentKim@users.noreply.github.com>
2021-02-02 17:34:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ var app = express();
 const bodyparser = require('body-parser');
 
 app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 
 var mysqlConnection = mysql.createConnection({
     host: "localhost",
     port: "3306",
     user: "root",
     password: "1234",
-    database: "UserDB"
+    database: "UserDB",
+    multipleStatements: true
 });
 
 mysqlConnection.connect((err) => {
@@ -18,10 +20,64 @@ mysqlConnection.connect((err) => {
     else console.log("DB connection failed \n Error: " + JSON.stringify(err, undefined, 2));
 });
 
-app.listen(300, () => console.log("Express Server os runnign at port no: 30000"));
-app.get("/users", (res, req) => {
+app.listen(3000, () => console.log("Express Server os running at port no: 3000"));
+app.get("/users", (req, res) => {
     mysqlConnection.query("SELECT * FROM TB_USER", (err, rows, fields)=>{
-        if(!err) console.log(rows);
+        if(!err) res.send(rows);
         else console.log(err);
     });
+});
+
+app.get("/users/:id", (req, res) => {
+    mysqlConnection.query("SELECT * FROM TB_USER WHERE ID = ?", [req.params.id], (err, rows, fields)=>{
+        if(!err) res.send(rows);
+        else console.log(err);
+    });
+});
+
+app.delete("/users/:id", (req, res) => {
+    mysqlConnection.query("DELETE FROM TB_USER WHERE ID = ?", [req.params.id], (err, rows, fields)=>{
+        if(!err) res.send(rows);
+        else console.log(err);
+    });
+});
+
+app.get('/', (req, res) => {
+    console.log(req.params);
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+});
+
+app.post("/users", (req, res) => {
+    // let user = req.params.user;
+    let values = req.body;
+    let user_name = req.body.USER_NAME;
+    // let user = req.body.TB_USER;
+    // let user_sql = `INSERT INTO TB_USER SET ? `;
+    console.log('req.body.ADDRESS: '+values);
+    // let user_sql = "INSERT INTO TB_USER(`USER_NAME`, `EMAIL`, `IMAGE_URL`, `ADDRESS`) VALUES ('${req.body.user_name}', '?', '?', '?')";
+    // let user_sql = "INSERT INTO TB_USER SET USER_NAME = '' ";
+    let user_sql = "INSERT INTO TB_USER SET USER_NAME = ?, EMAIL = ?, IMAGE_URL = ?, ADDRESS = ?";
+    mysqlConnection.query(user_sql, [values.USER_NAME, values.EMAIL,values.IMAGE_URL,values.ADDRESS], (err, rows, fields) => {
+        if(!err) res.send(rows);
+        else console.log(err);
+    })
+});
+
+app.put("/users", (req, res) => {
+    // let user = req.params.user;
+    let values = req.body;
+    let user_name = req.body.USER_NAME;
+    // let user = req.body.TB_USER;
+    // let user_sql = `INSERT INTO TB_USER SET ? `;
+    console.log('req.body.ADDRESS: '+values);
+    // let user_sql = "INSERT INTO TB_USER(`USER_NAME`, `EMAIaL`, `IMAGE_URL`, `ADDRESS`) VALUES ('${req.body.user_name}', '?', '?', '?')";
+    // let user_sql = "INSERT INTO TB_USER SET USER_NAME = '' ";
+    let user_sql = "UPDATE TB_USER SET USER_NAME = ?, EMAIL = ?, IMAGE_URL = ?, ADDRESS = ? WHERE ID = ?";
+    mysqlConnection.query(user_sql, [values.USER_NAME, values.EMAIL,values.IMAGE_URL,values.ADDRESS, values.ID], (err, rows, fields) => {
+        if(!err) res.send('Successfully updated the data');
+        else console.log(err);
+    })
 });
