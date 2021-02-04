9202c5ff5dac082da87d922c4ef6a3cc2bb3a600
GeonwooVincentKim <kdsnop@gmail.com>
2021-02-04 10:58:19 +0900

    Update codes

--- server.js
+++ server.js
@@ -9,13 +9,14 @@ app.use(bodyParser.json());
 
 // parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
-require("./app/routes/customer.routes.js")(app);
 
 // simple route
 app.get("/", (req, res) => {
     res.json({message: "Welcome to Geonwoo's application"});
 });
-
+// const routeFunction = require("./app/routes/customer.routes.js")(app);
+// routeFunction(app);
+require("./app/routes/customer.routes.js")(app);
 
 // set port, listen for requests
 app.listen(3005, () => {
